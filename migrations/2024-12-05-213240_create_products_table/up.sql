0ff0ca3d5658916541af42f6ffdff158f8c91be9
roney ruiz <roneyruizrojas@gmail.com>
2024-12-05 16:08:39 -0600

    feat: product table migration

---
+++ migrations/2024-12-05-213240_create_products_table/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  price NUMERIC NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
