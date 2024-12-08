efd9c3000a5eb6e143c8e1a52e00df3a445f6958
roney ruiz <roneyruizrojas@gmail.com>
2024-12-07 23:44:53 -0600

    feat: improve db context typing

--- crates/presentation/src/startup.rs
+++ crates/presentation/src/startup.rs
@@ -1,8 +1,9 @@
+/* use infrastructure::database::context::context; */
 use rocket::{routes, Rocket};
-use crate::{controllers::ping_controller::index, states::database::DatabaseState};
+use crate::controllers::ping_controller::index; //states::database::DatabaseState
 
 pub fn build_rocket() -> Rocket<rocket::Build> {
   rocket::build()
-    .manage(DatabaseState::new())
+    /* .manage(context()) */
     .mount("/api/ping", routes![index])
 }
